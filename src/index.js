20bec98879545ad361fa3d1740b446860e7d0627
Kim Carter <Kim.Carter@binarymist.net>
2018-06-10 14:26:59 +1200

    Linted src/index

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
-'use strict';
-
 const config = require('config/config');
-//const log = require('purpleteam-logger').init(config.get('logger'));
+// const log = require('purpleteam-logger').init(config.get('logger'));
 // Todo: KC: Setup logging
 const { processCommands } = require('src/cli');
-const { runScreen }  = require('src/screen');
+const { runScreen } = require('src/screen');
 
-exports.start = async function (options) {
-  
-  
-  debugger;
-  if (! await processCommands({argv: options.argv})) runScreen();
-  debugger;
-
-
+exports.start = async (options) => {
+  if (!await processCommands({ argv: options.argv })) runScreen();
 };
