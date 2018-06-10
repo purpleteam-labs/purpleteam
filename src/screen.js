df1c81d76b4639aee01c2ee781f6d862f137278b
Kim Carter <Kim.Carter@binarymist.net>
2018-06-10 14:20:23 +1200

    Linted src/screen

--- src/screen.js
+++ src/screen.js
@@ -1,24 +1,16 @@
-'use strict'
-
-const blessed = require('blessed')
-const contrib = require('blessed-contrib')
+const blessed = require('blessed');
+const contrib = require('blessed-contrib');
 
 let screen = undefined;
 let grid = undefined;
 
-
-
-const initScreen = () => {console.log('initialising screen');};
+const initScreen = () => { console.log('initialising screen'); }; // eslint-disable-line no-console
 
 const runScreen = () => {
-
   initScreen();
-
-
 };
 
 module.exports = {
-
   runScreen
 
 };
