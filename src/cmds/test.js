c74eeb9a84126d2363eca7fea59330b6d348204a
Kim Carter <Kim.Carter@binarymist.net>
2018-07-08 16:52:47 +1200

    Removed unused requires

--- src/cmds/test.js
+++ src/cmds/test.js
@@ -1,21 +1,21 @@
 const config = require('config/config');
 const log = require('purpleteam-logger').logger();
-const readFileAsync = require('util').promisify(require('fs').readFile);
-const request = require('request-promise-native');
 const api = require('src/apiDecoratingAdapter');
+
+api.init(log);
 
 exports.flags = 'test';
 exports.description = 'Launch purpleteam to attack your specified target';
 exports.setup = (sywac) => {
+  debugger;
   sywac.option(
     '-c, --config-file <config-file path>',
     {
       type: 'file', desc: 'Build user supplied configuration file. Must be a file conforming to the schema defined in the purpleteam documentation.', strinct: true, mustExist: true
     }
-  );
+  )
 };
 exports.run = async (parsedArgv, context) => {
-  const argv = parsedArgv;
   if (parsedArgv.c) {    
     const configFileContents = await api.getBuildUserConfigFile(parsedArgv.c);    
     // Todo: KC: In the future we could deserialise configFileContents, and possibly validate before sending to the Orchestrator.
@@ -27,8 +27,12 @@ exports.run = async (parsedArgv, context) => {
     await api.test(configFileContents);
 
 
+    // Use a setInterval or promise to keep the process running.
 
-
+      setInterval(() => {
+        console.log('This will not run');
+      }, 5000);
+    
 
 
 
@@ -46,12 +50,11 @@ exports.run = async (parsedArgv, context) => {
 
 
 
-
+debugger;
     // Todo: KC: Start the testing.
     log.notice('Ok, so test is running', { tags: ['test'] });
   } else {
     context.cliMessage('You must provide a valid build user configuration file that exists on the local file system.');
   }
 
-  argv.handled = true;
 };
