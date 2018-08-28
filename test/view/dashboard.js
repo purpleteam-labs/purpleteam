aa9aec3419d7b5cf0d3ea04d72c1ee5c5332a77d
Kim Carter <Kim.Carter@binarymist.net>
2018-08-28 15:41:59 +1200

    Removed commented out code

--- test/view/dashboard.js
+++ test/view/dashboard.js
@@ -20,27 +20,15 @@ describe('dashboard', () => {
     it('- testerType `app`, sessionId `lowPrivUser` should log provided message', (flags) => {
       const { context: { log0, log1 } } = flags;
       const rewiredDashboard = rewire('src/view/dashboard');
-      // const rewiredInternals = rewiredDashboard.__get__('internals');
-      // const expectedTesterSessions = [ // Taken from the model test
-      //   { testerType: 'app', sessionId: 'lowPrivUser', threshold: 12 }, 'App tests are now running.'
-      //   { testerType: 'app', sessionId: 'adminUser', threshold: 0 }, 'App tests are now running.'
-      //   { testerType: 'server', sessionId: 'NA', threshold: 0 }, 'No server testing available currently. The server tester is currently in-active.'
-      //   { testerType: 'tls', sessionId: 'NA', threshold: 0 }
-      // ];
-      // rewiredSetupInfoOutsForTest(expectedTesterSessions);
-
 
       const logger0 = { sessionId: 'lowPrivUser', instance: { log: log0 } };
       const logger1 = { sessionId: 'adminUser', instance: { log: log1 } };
       const testerType = 'app';
-      // rewiredInternals.infoOuts[testerType].loggers.push(logger0, logger1);
       const revertRewiredDashboardInternalsInfoOutsAppLoggers = rewiredDashboard.__set__(`internals.infoOuts.${testerType}.loggers`, [logger0, logger1]);
 
       rewiredDashboard.handleTesterProgress(testerType, 'lowPrivUser', 'App tests are now running.');
 
-      flags.onCleanup = () => {
-        revertRewiredDashboardInternalsInfoOutsAppLoggers();
-      };
+      flags.onCleanup = () => { revertRewiredDashboardInternalsInfoOutsAppLoggers(); };
 
       expect(log0.callCount).to.equal(1);
       expect(log1.callCount).to.equal(0);
@@ -51,27 +39,16 @@ describe('dashboard', () => {
     it('- testerType `app`, sessionId `adminUser` should log provided message', (flags) => {
       const { context: { log0, log1 } } = flags;
       const rewiredDashboard = rewire('src/view/dashboard');
-      // const rewiredInternals = rewiredDashboard.__get__('internals');
-      // const expectedTesterSessions = [ // Taken from the model test
-      //   { testerType: 'app', sessionId: 'lowPrivUser', threshold: 12 }, 'App tests are now running.'
-      //   { testerType: 'app', sessionId: 'adminUser', threshold: 0 }, 'App tests are now running.'
-      //   { testerType: 'server', sessionId: 'NA', threshold: 0 }, 'No server testing available currently. The server tester is currently in-active.'
-      //   { testerType: 'tls', sessionId: 'NA', threshold: 0 }
-      // ];
-      // rewiredSetupInfoOutsForTest(expectedTesterSessions);
-
 
       const logger0 = { sessionId: 'lowPrivUser', instance: { log: log0 } };
       const logger1 = { sessionId: 'adminUser', instance: { log: log1 } };
       const testerType = 'app';
-      // rewiredInternals.infoOuts[testerType].loggers.push(logger0, logger1);
+
       const revertRewiredDashboardInternalsInfoOutsAppLoggers = rewiredDashboard.__set__(`internals.infoOuts.${testerType}.loggers`, [logger0, logger1]);
 
       rewiredDashboard.handleTesterProgress(testerType, 'adminUser', 'App tests are now running.');
 
-      flags.onCleanup = () => {
-        revertRewiredDashboardInternalsInfoOutsAppLoggers();
-      };
+      flags.onCleanup = () => { revertRewiredDashboardInternalsInfoOutsAppLoggers(); };
 
       expect(log0.callCount).to.equal(0);
       expect(log1.callCount).to.equal(1);
