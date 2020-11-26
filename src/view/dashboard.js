739309e7be76c89fdce98e02ad63c5718e27ff3c
Kim Carter <Kim.Carter@binarymist.net>
2020-11-27 00:51:09 +1300

    Fixed bug where NaN was being displayed in dashboard for tester pct complete

--- src/view/dashboard.js
+++ src/view/dashboard.js
@@ -145,7 +145,7 @@ const handleTesterPctComplete = (testerType, sessionId, message) => {
   infoOuts[testerType].statTable.records.find((r) => r.sessionId === sessionId).pctComplete = Math.round(message);
   // testerPctComplete
   infoOuts[testerType].testerPctComplete.percent = infoOuts[testerType]
-    .statTable.records.reduce((accum, curr) => accum.pctComplete + curr.pctComplete) / infoOuts[testerType].statTable.records.length;
+    .statTable.records.reduce((accum, curr) => accum + curr.pctComplete, 0) / infoOuts[testerType].statTable.records.length;
   infoOuts[testerType].testerPctComplete.color = colourOfDonut(infoOuts[testerType].testerPctComplete.percent);
   // totalProgress
   const pctsComplete = [];
