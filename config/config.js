29c90323b7ff8b69f9e085da0b22bd455c979f12
Kim Carter <Kim.Carter@binarymist.net>
2022-05-08 15:44:20 +1200

    Bump job file version to keep up with release version

--- config/config.js
+++ config/config.js
@@ -214,8 +214,8 @@ const schema = {
     },
     version: {
       doc: 'The version of the Job accepted by the PurpleTeam API.',
-      format: ['0.1.0-alpha.1', '1.0.0-alpha.3', '2.0.0-alpha.3', '3.0.0-alpha.3', '3.1.0-alpha.3'],
-      default: '3.1.0-alpha.3'
+      format: ['0.1.0-alpha.1', '1.0.0-alpha.3', '2.0.0-alpha.3', '3.0.0-alpha.3', '3.1.0-alpha.3', '4.0.0-alpha.3'],
+      default: '4.0.0-alpha.3'
     }
   },
   modulePaths: {
